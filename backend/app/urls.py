874a0d3540bdacba4f7fed738106580dc69e12ef
Daniel James Williams <mail@danielwilliams.dk>
2015-11-05 17:46:30 +0100

    Card & Card Answer now take multiple HTTP methods

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 
 urlpatterns = [
     # API URLs
-    url(r'^api/cards/', 'card.views.get_card_list'),
-    url(r'^api/cards/(?P<card_id>[0-9]+)/', 'card.views.get_card_item'),
+    url(r'^api/cards/', 'card.views.card_list'),
+    url(r'^api/cards/(?P<card_id>[0-9]+)/', 'card.views.card_item'),
 
-    url(r'^api/card-answers/(?P<profile_id>[0-9]+)/', 'card.views.get_card_answer_list'),
-    url(r'^api/card-answers/(?P<card_answer_id>[0-9]+)/(?P<profile_id>[0-9]+)/', 'card.views.get_card_answer_item'),
+    url(r'^api/card-answers/(?P<profile_id>[0-9]+)/', 'card.views.card_answer_list'),
+    url(r'^api/card-answers/(?P<card_answer_id>[0-9]+)/(?P<profile_id>[0-9]+)/', 'card.views.card_answer_item'),
 
     # Admin URLs
     url(r'^admin/', include(admin.site.urls)),
--- backend/card/views/__init__.py
+++ backend/card/views/__init__.py
@@ -1,4 +1,45 @@
-from card.views.get_card_list import get_card_list
-from card.views.get_card_answer_list import get_card_answer_list
+from django.http import HttpResponse
+from card.views.card_list import get_card_list
+from card.views.card_item import get_card_item
+from card.views.card_answer_list import get_card_answer_list
+from card.views.card_answer_item import get_card_answer_item
 
-from card.views.get_card_item import get_card_item
+
+# ################
+# ##### CARD #####
+# ################
+
+def card_list(request):
+    if request.method == 'GET':
+        return get_card_list(request)
+    elif request.method == 'PUT':
+        # Status: Not Implemented
+        return HttpResponse(status=501)
+
+
+def card_item(request):
+    if request.method == 'GET':
+        return get_card_item(request)
+    elif request.method == 'PUT':
+        # Status: Not Implemented
+        return HttpResponse(status=501)
+
+
+# #######################
+# ##### CARD ANSWER #####
+# #######################
+
+def card_answer_list(request):
+    if request.method == 'GET':
+        return get_card_answer_list(request)
+    elif request.method == 'PUT':
+        # Status: Not Implemented
+        return HttpResponse(status=501)
+
+
+def card_answer_item(request):
+    if request.method == 'GET':
+        return get_card_answer_item(request)
+    elif request.method == 'PUT':
+        # Status: Not Implemented
+        return HttpResponse(status=501)
